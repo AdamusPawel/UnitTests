dc05f59b63168295e9834bc6d0138d44449b93d7
AdamusPawel <adamuspawel95@gmail.com>
2018-03-17 12:56:44 +0100

    Added both projectds

---
+++ UnitTests_PersonAdvanced/PersonAdvanced.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests_PersonAdvanced
+{
+    class PersonAdvanced
+    { 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public object Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public Address Address { get; set; }
+        public PersonAdvanced Spouse { get; set; }
+        public PersonAdvanced[] Children { get; set; }
+    }
+    class Gender
+    {
+        public static object Male { get; set; }
+        public static object Female { get; set; }
+    }
+    class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZIPCode { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public int LocalNumber { get; set; }
+    }
+}
---
+++ UnitTests_PersonAdvanced/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests_PersonAdvanced
+{
+    class Program
+    {
+        public static PersonAdvanced JohnSmith = new PersonAdvanced
+        {
+            Name = "John",
+            Surname = "Smith",
+            Gender = Gender.Male,
+            Birthday = new DateTime(1972, 2, 2),
+            Address = Address,
+            Spouse = SussaneSmith,
+            Children = new[] { MarrySmith, KewinSmith }
+        };
+
+        public static PersonAdvanced SussaneSmith = new PersonAdvanced
+        {
+            Name = "Sussane",
+            Surname = "Smith",
+            Gender = Gender.Female,
+            Birthday = new DateTime(1976, 6, 6),
+            Address = Address,
+            Spouse = JohnSmith,
+            Children = new[] { MarrySmith, KewinSmith }
+        };
+
+        public static PersonAdvanced KewinSmith = new PersonAdvanced
+        {
+            Name = "Kewin",
+            Surname = "Smith",
+            Gender = Gender.Male,
+            Birthday = new DateTime(2001, 1, 1),
+            Address = Address
+        };
+
+        public static PersonAdvanced MarrySmith = new PersonAdvanced
+        {
+            Name = "Mary",
+            Surname = "Smith",
+            Gender = Gender.Female,
+            Birthday = new DateTime(2002, 2, 2),
+            Address = Address
+        };
+
+        public static readonly Address Address = new Address
+        {
+            Country = "London",
+            City = "London",
+            ZIPCode = "W1T 1JY",
+            Street = "Tottenham Court Road",
+            StreetNumber = 14,
+            LocalNumber = 1
+        };
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
